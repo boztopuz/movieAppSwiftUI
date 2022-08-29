da4659bf3d7743d59954ff60ae03a362d9f58185
BurakOztopuz <boztopuz@gmail.com>
2022-08-29 13:19:10 +0300

    Initial Commit

---
+++ TheMovieApp/TheMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieAppApp.swift
+//  TheMovieApp
+//
+//  Created by Burak Öztopuz on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
